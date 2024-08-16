6b67d205243491ff06b707ce8181ed79c9b6c244
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-16 11:15:39 -0500

    Add difficulty levels in Rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use rand::prelude::SliceRandom;
 use rand::Rng;
 use wasm_bindgen::prelude::*;
 
 struct GameState {
     score: u32,                //game score
     multiplier: f32,           //score multiplier
+    difficuity: u32,           //game difficulty
     character_to_type: String, //character to type
 }
 impl GameState {
@@ -13,13 +15,15 @@ impl GameState {
         GameState {
             score: 0,                                                //set score to 0
             multiplier: 1.0,                                         //set multiplier to 1
+            difficuity: 1,                                           //set difficulty to 1
             character_to_type: rng.gen_range('a'..='z').to_string(), //set character to type to random character from character set
         }
     }
 
     fn update_score(&mut self, points: u32) {
         //update score
-        self.score += (points as f32 * self.multiplier) as u32; //increase score based on amount and multiplier
+        self.score += (points as f32 * self.multiplier * self.difficuity as f32).floor() as u32;
+        //increase score based on amount and multiplier
     }
 
     fn set_multiplier(&mut self, new_multiplier: f32) {
@@ -40,7 +44,68 @@ fn handle_typing(game_state: &mut GameState, typed_key: String) {
         game_state.update_score(100); //increase points
         game_state.set_multiplier(game_state.multiplier + 0.01 / game_state.multiplier.max(1.0)); //increase score multiplier
         let mut rng: rand::prelude::ThreadRng = rand::thread_rng(); //random object
-        game_state.set_character_to_type(rng.gen_range('a'..='z').to_string()); //set character to type to random character from character list
+        let lowercase_characters: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect(); //lowercase characters
+        let uppercase_characters: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect(); //uppercase characters
+        let numeric_digit_characters: Vec<char> = "0123456789".chars().collect(); //numeric digits
+        let symbol_characters: Vec<char> = "~!@#$%^&*()_+`-=[]\\;',./{}|:\"<>?".chars().collect(); //special characters (symbols)
+        if game_state.difficuity == 1 {
+            game_state
+                .set_character_to_type(lowercase_characters.choose(&mut rng).unwrap().to_string());
+        //set character to type to random lowercase character from character list
+        } else if game_state.difficuity == 2 {
+            let rand_value: f32 = rng.gen();
+            if rand_value < 1.0 / 2.0 {
+                game_state.set_character_to_type(
+                    lowercase_characters.choose(&mut rng).unwrap().to_string(),
+                ); //set character to type to random lowercase character from character list
+            } else {
+                game_state.set_character_to_type(
+                    uppercase_characters.choose(&mut rng).unwrap().to_string(),
+                )
+            }; //set character to type to random uppercase character from character list
+        } else if game_state.difficuity == 3 {
+            let rand_value: f32 = rng.gen();
+            if rand_value < 26.0 / 62.0 {
+                game_state.set_character_to_type(
+                    lowercase_characters.choose(&mut rng).unwrap().to_string(),
+                ); //set character to type to random lowercase character from character list
+            }
+            if rand_value < 52.0 / 62.0 {
+                game_state.set_character_to_type(
+                    uppercase_characters.choose(&mut rng).unwrap().to_string(),
+                ); //set character to type to random uppercase character from character list
+            } else {
+                game_state.set_character_to_type(
+                    numeric_digit_characters
+                        .choose(&mut rng)
+                        .unwrap()
+                        .to_string(),
+                )
+            }; //set character to type to random numeric digit character from character list
+        } else if game_state.difficuity == 4 {
+            let rand_value: f32 = rng.gen();
+            if rand_value < 26.0 / 94.0 {
+                game_state.set_character_to_type(
+                    lowercase_characters.choose(&mut rng).unwrap().to_string(),
+                ); //set character to type to random lowercase character from character list
+            }
+            if rand_value < 52.0 / 94.0 {
+                game_state.set_character_to_type(
+                    uppercase_characters.choose(&mut rng).unwrap().to_string(),
+                ); //set character to type to random uppercase character from character list
+            }
+            if rand_value < 62.0 / 94.0 {
+                game_state.set_character_to_type(
+                    numeric_digit_characters
+                        .choose(&mut rng)
+                        .unwrap()
+                        .to_string(),
+                ); //set character to type to random numeric digit character from character list
+            } else {
+                game_state
+                    .set_character_to_type(symbol_characters.choose(&mut rng).unwrap().to_string())
+            }; //set character to type to random symbol character from character list
+        }
     } else {
         game_state.set_multiplier(game_state.multiplier * 0.99); //decrease score multiplier
     }
