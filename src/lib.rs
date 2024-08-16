838a961a94a4daae1c12954c7e1da706c499700e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-16 11:22:28 -0500

    Fix score multiplier bonus increase calculation

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,12 @@ fn handle_typing(game_state: &mut GameState, typed_key: String) {
     if check_typing(game_state, typed_key) {
         //check if entered text is correct (entered text is equal to character to type)
         game_state.update_score(100); //increase points
-        game_state.set_multiplier(game_state.multiplier + 0.01 / game_state.multiplier.max(1.0)); //increase score multiplier
+        let multiplier_bonus: f32 = if game_state.multiplier < 1.0 {
+            0.02 - game_state.multiplier * 0.01
+        } else {
+            0.01 / game_state.multiplier.max(1.0)
+        }; //increase score multiplier based on currect score multiplier
+        game_state.set_multiplier(game_state.multiplier + multiplier_bonus); //increase score multiplier
         let mut rng: rand::prelude::ThreadRng = rand::thread_rng(); //random object
         let lowercase_characters: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect(); //lowercase characters
         let uppercase_characters: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect(); //uppercase characters
